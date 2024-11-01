644009bc57eac5a96bce33e0bb57d70242978645
Leo <dev@tjlabscorp.com>
2024-11-01 14:15:15 +0900

    0.1.9

--- OlympusSDK.podspec
+++ OlympusSDK.podspec
@@ -1,7 +1,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'OlympusSDK'
-  s.version          = '0.1.8'
+  s.version          = '0.1.9'
   s.summary          = 'OlympusSDK for iOS'
   s.swift_version    = '5.0'
   
--- OlympusSDK/Classes/OlympusServiceManager.swift
+++ OlympusSDK/Classes/OlympusServiceManager.swift
@@ -2,7 +2,7 @@ import Foundation
 import UIKit
 
 public class OlympusServiceManager: Observation, StateTrackingObserver, BuildingLevelChangeObserver {
-    public static let sdkVersion: String = "0.1.8"
+    public static let sdkVersion: String = "0.1.9"
     var isSimulationMode: Bool = false
     var bleFileName: String = ""
     var sensorFileName: String = ""
