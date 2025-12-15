f9d6234b1393c47698b89bc0dbbc3ae713ce43d0
tjlabs-dev <92098727+tjlabs-dev@users.noreply.github.com>
2025-12-15 14:42:40 +0900

    build: update framework
    framework

---
+++ OlympusSDK/Frameworks/TJLabsAuth.xcframework/ios-arm64_x86_64-simulator/TJLabsAuth.framework/Headers/TJLabsAuth.h
@@ -0,0 +1,18 @@
+//
+//  TJLabsAuth.h
+//  TJLabsAuth
+//
+//  Created by 신동현 on 5/13/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TJLabsAuth.
+FOUNDATION_EXPORT double TJLabsAuthVersionNumber;
+
+//! Project version string for TJLabsAuth.
+FOUNDATION_EXPORT const unsigned char TJLabsAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TJLabsAuth/PublicHeader.h>
+
+
