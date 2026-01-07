abde748c758899500f2c3008257bfaf46adb3aa7
tjlabs-dev <92098727+tjlabs-dev@users.noreply.github.com>
2026-01-07 19:27:45 +0900

    Update LandmarkTagger.swift
    init LandmarkTagger

--- OlympusSDK/Classes/LandmarkTagger.swift
+++ OlympusSDK/Classes/LandmarkTagger.swift
@@ -1,8 +1,28 @@
-//
-//  LandmarkTagger.swift
-//  OlympusSDK
-//
-//  Created by leo.shin on 12/12/25.
-//
 
 import Foundation
+import TJLabsCommon
+import TJLabsResource
+
+class LandmarkTagger {
+    init(sectorId: Int) {
+        self.sectorId = sectorId
+    }
+    
+    var sectorId: Int
+    var landmarkData = [String: [String: LandmarkData]]()
+    
+    func setLandmarkData(key: String, data: [String: LandmarkData]) {
+        self.landmarkData[key] = data
+    }
+    
+    func findMatchedLandmarkWithUserPeak(userPeak: UserPeak, curResult: FineLocationTrackingOutput?) -> LandmarkData? {
+        guard let curResult = curResult else { return nil }
+        let key = "\(sectorId)_\(curResult.building_name)_\(curResult.level_name)"
+        guard let landmarkData = self.landmarkData[key] else { return nil }
+        guard let matchedLandmark = landmarkData[userPeak.id] else { return nil }
+        
+        JupiterLogger.i(tag: "LandmarkTagger", message: "(findMatchedLandmarkWithUserPeak) matchedLandmark: \(matchedLandmark)")
+        
+        return matchedLandmark
+    }
+}
