00ca66de5a702fe9c9aa9bb5ada5238a31c32c28
tjlabs-dev <92098727+tjlabs-dev@users.noreply.github.com>
2026-02-05 16:08:35 +0900

    Update OlympusNetworkManager.swift
    add mobile event post function

--- OlympusSDK/Classes/OlympusNetworkManager.swift
+++ OlympusSDK/Classes/OlympusNetworkManager.swift
@@ -1141,6 +1141,56 @@ public class OlympusNetworkManager {
         }
     }
     
+    func postMobileEvent(url: String, input: MobileEvent, completion: @escaping (Int, String) -> Void) {
+        // [http 비동기 방식을 사용해서 http 요청 수행 실시]
+        let urlComponents = URLComponents(string: url)
+        var requestURL = URLRequest(url: (urlComponents?.url)!)
+        
+        requestURL.httpMethod = "POST"
+        let encodingData = JSONConverter.encodeJson(param: input)
+        if (encodingData != nil) {
+            requestURL.httpBody = encodingData
+            requestURL.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            requestURL.setValue("\(encodingData)", forHTTPHeaderField: "Content-Length")
+            
+            let dataTask = self.reportSession.dataTask(with: requestURL, completionHandler: { (data, response, error) in
+                // [error가 존재하면 종료]
+                guard error == nil else {
+                    // [콜백 반환]
+                    completion(500, error?.localizedDescription ?? "Fail")
+                    return
+                }
+                
+                // [status 코드 체크 실시]
+                let successsRange = 200..<300
+                guard let statusCode = (response as? HTTPURLResponse)?.statusCode, successsRange.contains(statusCode)
+                else {
+                    // [콜백 반환]
+                    completion(500, (response as? HTTPURLResponse)?.description ?? "Fail")
+                    return
+                }
+                
+                // [response 데이터 획득]
+                let resultCode = (response as? HTTPURLResponse)?.statusCode ?? 500 // [상태 코드]
+                guard let resultLen = data else {
+                    completion(500, (response as? HTTPURLResponse)?.description ?? "Fail")
+                    return
+                }
+                let resultData = String(data: resultLen, encoding: .utf8) ?? "" // [데이터 확인]
+                
+                // [콜백 반환]
+                DispatchQueue.main.async {
+                    completion(resultCode, resultData)
+                }
+            })
+            
+            // [network 통신 실행]
+            dataTask.resume()
+        } else {
+            completion(500, "Fail to encode")
+        }
+    }
+    
     func getBlackList(url: String, completion: @escaping (Int, String) -> Void) {
         var urlComponents = URLComponents(string: url)
         var requestURL = URLRequest(url: (urlComponents?.url)!)
