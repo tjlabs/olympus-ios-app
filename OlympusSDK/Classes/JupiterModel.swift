4e4be455ab25ca4016b3ab95bd655690b86cfaec
tjlabs-dev <92098727+tjlabs-dev@users.noreply.github.com>
2026-03-24 19:06:58 +0900

    Update JupiterModel.swift
    - add models
    - edit JupiterDebugResult

--- OlympusSDK/Classes/JupiterModel.swift
+++ OlympusSDK/Classes/JupiterModel.swift
@@ -55,7 +55,7 @@ public struct LLH: Codable {
 }
 
 // MARK: - JupiterDebugResult
-public struct JupiterDebugResult: Codable {
+public struct JupiterDebugResult {
     public var mobile_time: Int
     public var building_name: String
     public var level_name: String
@@ -79,6 +79,8 @@ public struct JupiterDebugResult: Codable {
     public var recon_raw_traj: [[Double]]?
     public var recon_corr_traj: [FineLocationTrackingOutput]?
     public var recovery_result: RecoveryResult?
+    public var selected_cand: SelectedCandidate?
+    public var selected_search: SelectedSearch?
     public var recovery_result3Peaks: RecoveryResult3Peaks?
     public var ratio: Float?
     public var navi_route: [NavigationRoute]?
@@ -112,6 +114,63 @@ public struct RecoveryResult: Codable {
         self.curLinkNum = curLinkNum
         self.curGroupNum = curGroupNum
     }
+}
+
+public struct SearchResult {
+    public let older: PeakData?
+    public let recent: PeakData?
+    public let traj: [CandidateTrajectory]
+    public let tail: ixyhs
+    public let head: ixyhs
+    public let headResult: FineLocationTrackingOutput
+    
+    public let lossPointResultList: [LossPointResult]
+    public let loss_lm: Float
+    public let loss_g_d: Float
+    public let loss_g_h: Float
+}
+
+public struct SelectedSearch {
+    public let older: PeakData?
+    public let recent: PeakData?
+    public let traj: [CandidateTrajectory]
+    public let tail: ixyhs
+    public let head: ixyhs
+    public var headResult: FineLocationTrackingOutput
+    
+    public let loss: Float
+}
+
+public struct CandidateResult {
+    public let older: PeakData?
+    public let recent: PeakData?
+    public let links: [Int]
+    public let linkGroups: Set<Int>
+    public let traj: [CandidateTrajectory]
+    public let tail: ixyhs
+    public let head: ixyhs
+    public let headResult: FineLocationTrackingOutput
+    public let isInSameLinkGroup: Bool
+    public let linkGroupSwitchCount: Int
+    public let distWithRecentPeakResult: Float?
+    
+    public let lossPointResultList: [LossPointResult]
+    public let loss_lm: Float
+    public let loss_g_d: Float
+    public let loss_g_h: Float
+}
+
+public struct SelectedCandidate {
+    public let older: PeakData?
+    public let recent: PeakData?
+    public let links: [Int]
+    public let linkGroups: Set<Int>
+    public let traj: [CandidateTrajectory]
+    public let tail: ixyhs
+    public let head: ixyhs
+    public let headResult: FineLocationTrackingOutput
+
+    public let loss: Float
 }
 
 public struct RecoveryResult3Peaks: Codable {
